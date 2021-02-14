adbf7051c2ce5606cf49072ff80221ada6fecdf2
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-02-14 12:28:35 -0500

    Conf Databinding

--- app/src/main/java/com/example/databasetest20210213/MainActivity.kt
+++ app/src/main/java/com/example/databasetest20210213/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.databasetest20210213
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.databasetest20210213.database.UserDatabase
+import com.example.databasetest20210213.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         val application = requireNotNull(this).application
         val dataSource = UserDatabase.getInstance(application).userDatabaseDao
@@ -20,5 +23,8 @@ class MainActivity : AppCompatActivity() {
         mainViewModel = ViewModelProvider(this, viewModelFactory)
             .get(MainViewModel::class.java)
 
+        binding.mainViewModel = mainViewModel
+
+        binding.lifecycleOwner = this
     }
 }
