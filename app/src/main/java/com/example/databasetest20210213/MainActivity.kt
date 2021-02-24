a8f949c7286c75b2facfac099bae6301a5589290
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-02-23 22:40:28 -0500

    Hide Keyboard_Validate Data

--- app/src/main/java/com/example/databasetest20210213/MainActivity.kt
+++ app/src/main/java/com/example/databasetest20210213/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.databasetest20210213
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.databasetest20210213.database.UserDatabase
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val application = requireNotNull(this).application
         val dataSource = UserDatabase.getInstance(application).userDatabaseDao
@@ -28,10 +31,37 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
         binding.saveButton.setOnClickListener {
-            mainViewModel.onSaveUser(
-                binding.nameEdit.text.toString(),
-                binding.passwordEdit.text.toString()
-            )
+            if (validateData()) {
+                mainViewModel.onSaveUser(
+                    binding.nameEdit.text.toString(),
+                    binding.passwordEdit.text.toString()
+                )
+                binding.nameEdit.text.clear()
+                binding.passwordEdit.text.clear()
+            }
         }
+    }
+
+    private fun validateData(): Boolean {
+        if (binding.nameEdit.text.toString().trim().isEmpty()) {
+            hideKeyboard(binding.nameEdit.windowToken)
+            binding.nameEdit.text.clear()
+            Toast.makeText(this, "Debe ingresar un nombre", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (binding.passwordEdit.text.toString().trim().isEmpty()) {
+            hideKeyboard(binding.passwordEdit.windowToken)
+            binding.passwordEdit.text.clear()
+            Toast.makeText(this, "Debe ingresar un password", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    private fun hideKeyboard(windowToken: android.os.IBinder) {
+        // Hide the keyboard.
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
